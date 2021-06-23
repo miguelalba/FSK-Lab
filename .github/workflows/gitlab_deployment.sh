36a775e7186cc551c73ff48fbb121f3c4f58f55c
thsch <thsch@DESKTOP-5Q5QQ7E>
2021-06-23 12:13:46 +0200

    find target folder

--- .github/workflows/gitlab_deployment.sh
+++ .github/workflows/gitlab_deployment.sh
@@ -1,17 +1,17 @@
 TARGET_FOLDER="$GITHUB_WORKSPACE/de.bund.bfr.knime.update/target"
 REPO="development"
-
+ls $TARGET_FOLDER
 # Check Gitlab repo
-git clone https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git
+#git clone https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git
 
 # Update build
-rm -Rf $REPO/fsklab # Deletes old build if it exists
+#rm -Rf $REPO/fsklab # Deletes old build if it exists
 
-mv $TARGET_FOLDER/repository $TARGET_FOLDER/fsklab
-mv $TARGET_FOLDER/fsklab $REPO/fsklab
-cd $REPO/fsklab
-git add .
-git commit -m "Development"
+#mv $TARGET_FOLDER/repository $TARGET_FOLDER/fsklab
+#mv $TARGET_FOLDER/fsklab $REPO/fsklab
+#cd $REPO/fsklab
+#git add .
+#git commit -m "Development"
 
 # Push build
-git push https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git --all
+#git push https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git --all

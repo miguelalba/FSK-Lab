d96dd077c69fd45e7bfc566ea7e160fee5589578
thsch <thsch@DESKTOP-5Q5QQ7E>
2021-06-29 10:51:47 +0200

    testing isa4j library

---
+++ de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/ISA.java
@@ -0,0 +1,12 @@
+package de.bund.bfr.knime.fsklab;
+
+import de.ipk_gatersleben.bit.bi.isa4j.components.Assay;
+
+public class ISA {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Assay a = new Assay("");
+  }
+
+}
